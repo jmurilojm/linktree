d3a3f9ba1b4e06a0f9ff74a525f36202e0a9f717
JMurilo <j.murilo.87@gmail.com>
2024-02-23 15:13:20 -0300

    update app.js - acreacentados comentarios e ajustes.

--- pagina-de-projetos/projetos/sorteio/app.js
+++ pagina-de-projetos/projetos/sorteio/app.js
@@ -14,6 +14,14 @@ btAdicionar.addEventListener('click', adicionar);
 btSortear.addEventListener('click', sortear);
 btNovoSorteio.addEventListener('click', novoSorteio);
 btLimpar.addEventListener('click', limpar);
+inNome.addEventListener('keydown', (e) => {
+  if(e.keyCode === 13){
+    adicionar();
+  } // caso o botao enter, que possui o codigo 13, for clicado, a funcao adionar é chamada.
+});
+quadroEntrada.addEventListener('click',()=>{
+  inNome.focus();
+}); // caso clique na area onde fica a lista de nomes cadastrados, será direcionado focus() no input-text de adicionar nomes;
 
 
 inNome.focus();
@@ -21,72 +29,73 @@ inNome.focus();
 function adicionar(){
   if(inNome.value === '' || inNome.value.length < 3){
     alert('Valor inválido ou muito pequeno!');
-    return
-  }
+    return;
+  } // verifica se o valor de entrada está vazio ou se têm menos que 03 caracteres. Se isso ocorrer, uma mensagem de alerta será mostrada.
   
   const nome = (inNome.value).trim().toUpperCase();
-  nomes.push(nome);
+  nomes.push(nome); // adiciona o nome digitado à lista nomes.
   inNome.value = '';
   inNome.focus();
   listarNomes();
   
-  // habilitar botao limpar.
+  // habilitar botão limpar. Por padrão está desabilitado.
     // remover esses atributos.
     btLimpar.removeAttribute('disabled');
     btLimpar.classList.remove('limparDesabilitado');
 }
 
 function listarNomes(){
-  quadroEntrada.innerText = '';
+  quadroEntrada.innerText = ''; // limpa o quadro dos nomes antes de fazer a atualização a seguir.
+  
   nomes.forEach(nome => {
     quadroEntrada.innerHTML += `${nome}<br>`;
-  })
+  }); // percorre a lista de nomes para apresentar cada um deles.
 }
 
 function sortear(){
   if(nomes.length === 0){
-    alert('Não há Nomes para sorteio!');
+    alert('Lista de Nomes está vazia!');
     cont = 1;
-    return
-  }
+    return;
+  } // antes de realizar o sorteio é verificado se existe nomes na lista de nomes. Caso não, um alerta será dado para que cadastre e possa seguir para as próximas alternativas.
   
-  const nomeSorteado = Math.floor(Math.random() * nomes.length);
+  const nomeSorteado = Math.floor(Math.random() * nomes.length); // realiza o sorteio de um indice da lista de nomes.
   nomes = nomes.filter((nome, i) => {
     if(nome !== nomes[nomeSorteado]){
       return nome;
     } else{
-      quadroSaida.innerHTML += `${cont}º - ${nomes[nomeSorteado]}<br>`;
+      quadroSaida.innerHTML += `${cont}º- ${nomes[nomeSorteado]}<br>`;
     }
-  });
-  cont++;
-  listarNomes();
+  }); // o nome sorteado será sera retirado da lista de nomes e passará para o quadro dos sorteados.
+  cont++; // para adicionar a colocação do sorteado no quadro dos sorteados.
+  listarNomes(); // atualiza a lista no quadro dos nomes.
   
-  // habilitar botao novo sorteio.
-  btNovoSorteio.removeAttribute('disabled');
-  btNovoSorteio.classList.remove('novoSorteioDesabilitado');
+  // habilita o botão novo sorteio, assim que realizar o primeiro sorteio.
+    btNovoSorteio.removeAttribute('disabled');
+    btNovoSorteio.classList.remove('novoSorteioDesabilitado');
   
-  // se for para habilitar o botao quando zerar a lista dos nomes cadastrados.
+  // se for para habilitar o botão quando zerar a lista dos nomes cadastrados.
   /*if(nomes.length === 0){
     // remover esses atributos.
     btNovoSorteio.removeAttribute('disabled');
     btNovoSorteio.classList.remove('novoSorteioDesabilitado');
   }*/
   
-  // apos o primeiro sorteio, o botao limpar fica indisponivel.
-    // retorna os atributos removidos do botao limpar.
+  // após o primeiro sorteio, o botão limpar fica indisponível.
+    // retorna os atributos removidos do botão limpar.
     btLimpar.setAttribute('disabled', true);
     btLimpar.classList.add('limparDesabilitado');
 }
 
 function novoSorteio(){
-  nomes = [];
-  cont = 1;
-  listarNomes();
-  quadroSaida.innerText = '';
+  nomes = []; // lista nomes volta a ficar vazia.
+  cont = 1; // resta o contador para 1.
+  listarNomes(); // atualiza o quadroNomes.
+  quadroSaida.innerText = ''; // quadro dos sorteados fica vazio.
   
   // retorna os atributos removidos do botao novo sorteio.
-  btNovoSorteio.setAttribute('disabled', true);
-  btNovoSorteio.classList.add('novoSorteioDesabilitado');
+    btNovoSorteio.setAttribute('disabled', true);
+    btNovoSorteio.classList.add('novoSorteioDesabilitado');
 }
 
 function limpar(){
@@ -96,6 +105,6 @@ function limpar(){
   quadroEntrada.innerText = '';
   
   // retorna os atributos removidos do botao limpar.
-  btLimpar.setAttribute('disabled', true);
-  btLimpar.classList.add('limparDesabilitado');
+    btLimpar.setAttribute('disabled', true);
+    btLimpar.classList.add('limparDesabilitado');
 }
