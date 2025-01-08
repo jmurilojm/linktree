888eba780af14a20481c67f8d9a9fa31d17ecea0
JMurilo <j.murilo.87@gmail.com>
2025-01-08 19:19:58 -0300

    update- melhoramento na implementação da lógica e refatoração

--- src/projects/sorteio-facil/script.js
+++ src/projects/sorteio-facil/script.js
@@ -1,69 +1,72 @@
-document.querySelector('#inNome').addEventListener('keydown', (e) => {
+const inNome = document.querySelector('#inNome')
+const outLista = document.querySelector('#outLista')
+const btnRealizarSorteio = document.querySelector('#btnRealizarSorteio')
+const btnAdicionarNome = document.querySelector('#btnAdicionarNome')
+const btnReiniciar = document.querySelector('#btnReiniciar')
+const nomeGanhador = document.querySelector('#nomeGanhador')
+
+let listaDeNomes = []
+inNome.focus()
+
+// usar tecla enter para inserção 
+inNome.addEventListener('keydown', (e) => {
   if(e.keyCode === 13){
-    adicionarNome()
+    addNomeNaLista()
   }
 })
 
-function adicionarNome() {
-  // pegar o texto informado e criar um elemento 'li' que ira receber o texto
-  let inNome = document.querySelector('#inNome')
-  let outLista = document.querySelector('#outLista')
-  let nome = document.createElement('li')
 
-  // eliminar os espaços e verificar se foi informado algum nome valido
-  inNome.value = inNome.value.trim()
-
-  if (!inNome.value) {
-    alert('Informe um nome!')
-    inNome.focus()
+function addNomeNaLista(){
+  // verificar se há dígito válido antes de adicionar
+  if(inNome.value.trim() === '' || inNome.value.length < 2){
+    alert('Valor inválido!')
     return
-  } else {
-    nome.textContent = inNome.value
-    outLista.appendChild(nome)
-
-    inNome.value = ''
-    inNome.focus()
   }
-
-  // habilitar botao de realizar sorteio, apos inclusao de pelo menos dois nomes
-  let nomes = document.querySelector('#outLista')
-  let quantidade = nomes.children.length
-
-  if (quantidade >= 2) {
-    document.querySelector('#btnRealizarSorteio').removeAttribute('disabled')
-  }
+  
+  // adicionar nome na lista
+  listaDeNomes.push(inNome.value.trim())
+  // limpar lista exibida para atualizar
+  outLista.innerHTML = ''
+  // imprimir lista atraves da função 
+  imprimirLista()
+  // limpar campo input
+  inNome.value = ''
+  inNome.focus()
 }
 
-function realizarSorteio() {
-  // pegar a quantidade de elementos inseridos e, pos confirmacao, realizar o sorteio
-  let nomes = document.querySelector('#outLista')
-  let quantidade = nomes.children.length
+function imprimirLista(){
+  // iterar nomes da lista
+  listaDeNomes.forEach(n => {
+    // lançar cada nome com a tag <li>
+    outLista.innerHTML += `<li>${n}</li>`
+  })
+}
 
-  let confirmacao = confirm('Sortear?')
-
-  if (confirmacao) {
-    let sorteado = Math.floor(Math.random() * quantidade)
-    let nomeSorteado = nomes.children[sorteado].textContent
-    
-    // estilizando o sorteado
-    nomes.children[sorteado].style.backgroundColor = 'coral'
-
-    let nomeGanhador = document.querySelector('#nomeGanhador')
-    nomeGanhador.textContent = nomeSorteado
-
-    // desabilitar botoes
-    document.querySelector('#btnRealizarSorteio').setAttribute('disabled', 'disabled')
-    document.querySelector('#btnAdicionarNome').setAttribute('disabled', 'disabled')
-    document.querySelector('#inNome').setAttribute('disabled', 'disabled')
-
-    // habilitar botao
-    document.querySelector('#btnReiniciar').removeAttribute('disabled')
+function sortearNome(){
+  // verificar se há nomes suficientes
+  if(listaDeNomes.length < 2){
+    alert('Cadastre pelo menos dois Nomes!')
+    return
   }
+  
+  // pegar total de nomes salvos
+  let quantidade = listaDeNomes.length
+  // sorteio de uma posição aleatória
+  let sorteado = Math.floor(Math.random() * quantidade)
+  // o ganhador é quem esta na posição aleatória 
+  let ganhador = listaDeNomes[sorteado]
+  
+  // exibir ganhador na tela
+  nomeGanhador.innerHTML = `${ganhador}`
+  // remover o sorteado das listas e atualizar
+  const liSorteado = outLista.children[sorteado]
+  outLista.removeChild(liSorteado)
+  listaDeNomes.splice(sorteado,1)
 }
 
 function reiniciar() {
   // apos confirmacao, atualizar a pagina para um novo sorteio
-  let confirmacao = confirm('Realizar Novo Sorteio?')
+  let confirmacao = confirm('Atencao!\n\nTodos os dados serão apagados.')
 
   if (confirmacao) {
     window.location.reload()
