303fc2f0f64d1836f87a631ee56252d5159f9f53
JMurilo <j.murilo.87@gmail.com>
2025-01-12 22:02:48 -0300

    update- contagem de adição

--- src/projects/sorteio-facil/script.js
+++ src/projects/sorteio-facil/script.js
@@ -1,12 +1,13 @@
 const inNome = document.querySelector('#inNome');
 const outLista = document.querySelector('#outLista');
-const btnRealizarSorteio = document.querySelector('#btnRealizarSorteio');
-const btnAdicionarNome = document.querySelector('#btnAdicionarNome');
+const btnSortear = document.querySelector('#btnSortear');
+const btnAdicionar = document.querySelector('#btnAdicionar');
 const btnReiniciar = document.querySelector('#btnReiniciar');
 const ganhador = document.querySelector('#ganhador');
 const numeroDoSorteio = document.querySelector('#numeroDoSorteio');
 const outData = document.querySelector('#outData');
 const outHora = document.querySelector('#outHora');
+const totalNaLista = document.querySelector('#totalNaLista')
 
 let listaDeNomes = [];
 inNome.focus();
@@ -19,19 +20,19 @@ setInterval(() => {
 
 // usar tecla enter para inserção de dados
 inNome.addEventListener('keydown', (e) => {
-  if(e.keyCode === 13){
+  if (e.keyCode === 13) {
     addNomeNaLista();
   }
 });
 
 
-function addNomeNaLista(){
+function addNomeNaLista() {
   // verificar se há dígito válido antes de adicionar
-  if(inNome.value.trim() === '' || inNome.value.trim().length < 2){
+  if (inNome.value.trim() === '' || inNome.value.trim().length < 2) {
     alert('Valor inválido!');
     return;
   }
-  
+
   // adicionar nome na lista
   listaDeNomes.push(inNome.value.trim());
   // limpar lista exibida para atualizar
@@ -41,9 +42,11 @@ function addNomeNaLista(){
   // limpar campo input
   inNome.value = '';
   inNome.focus();
+  // imformar total de cadastros
+  totalNaLista.textContent = listaDeNomes.length;
 }
 
-function imprimirLista(){
+function imprimirLista() {
   // iterar nomes da lista
   listaDeNomes.forEach(nome => {
     // lançar cada nome com a tag <li>
@@ -51,39 +54,39 @@ function imprimirLista(){
   });
 }
 
-function sortearNome(){
+function sortearNome() {
   // verificar se há nomes suficientes
-  if(listaDeNomes.length < 2){
+  if (listaDeNomes.length < 2) {
     alert('Cadastre pelo menos dois Nomes!');
     return;
   }
-  
+
   // pegar total de nomes salvos
   let quantidadeDeNomes = listaDeNomes.length;
   // sorteio de uma posição aleatória
   let numeroAleatorio = Math.floor(Math.random() * quantidadeDeNomes);
   // o ganhador é quem esta na posição aleatória 
   let nomeDoGanhador = listaDeNomes[numeroAleatorio];
-  
+
   // exibir ganhador na tela
   ganhador.innerHTML = `Parabéns ${nomeDoGanhador}!`;
   // remover o sorteado das listas e atualizar
   const liSorteada = outLista.children[numeroAleatorio];
   outLista.removeChild(liSorteada);
-  listaDeNomes.splice(numeroAleatorio,1);
-  
+  listaDeNomes.splice(numeroAleatorio, 1);
+
   // contagem de sorteio
   sorteios++;
   numeroDoSorteio.textContent = sorteios;
-  
+
   // atualizar hora
   //outHora.textContent = inforHora();
 }
 
-function inforData(){
+function inforData() {
   const data = new Date();
   const dia = acrescentarZero(data.getDate());
-  const mes = acrescentarZero(data.getMonth()+1);
+  const mes = acrescentarZero(data.getMonth() + 1);
   const ano = data.getFullYear();
   return `${dia}/${mes}/${ano}`;
 }
@@ -96,24 +99,25 @@ function inforHora() {
   return `${hora}:${min}:${seg}`;
 }
 
-function acrescentarZero(elemento){
+function acrescentarZero(elemento) {
   let formatado = elemento < 10 ? `0${elemento}` : elemento;
   return formatado;
 }
 
 function reiniciar() {
   let confirmacao = confirm('Atenção!\n\nTodos os dados serão apagados.');
-  
+
   // apos confirmacao, atualizar a pagina para um novo sorteio
   if (confirmacao) {
     listaDeNomes = [];
     sorteios = 0;
     outLista.innerHTML = '';
-    ganhador.innerHTML = '';
+    ganhador.innerHTML = '...';
     numeroDoSorteio.textContent = sorteios;
     inNome.value = '';
     inNome.focus();
     outData.textContent = inforData();
     outHora.textContent = inforHora();
+    totalNaLista.textContent = listaDeNomes.length;
   }
 }
